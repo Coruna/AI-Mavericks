6eb7565bf6c1aa29e2a3c490e57e2d1958a5f4ae
TheKillerPepsi@github.com <pia@pepsisblog.de>
2023-05-06 14:43:18 +0200

    Added long messages to response

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+#Note: The openai-python library support for Azure OpenAI is in preview.
+import os
+import openai
+openai.api_type = "azure"
+openai.api_base = "https://gpt-playground-us.openai.azure.com/"
+openai.api_version = "2023-03-15-preview"
+openai.api_key = "cXXXXXXX"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+response = openai.ChatCompletion.create(
+    engine="chatgpt", # engine = "deployment_name".
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
+        {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
+        {"role": "user", "content": "Do other Azure Cognitive Services support this too?"},
+        {"role": "assistant", "content": "Yes, other Azure Cognitive Services also support customer managed keys. Azure Cognitive Services is designed to allow you to use your own encryption keys for protecting your data at rest. By using customer managed keys, you can have greater control over the keys that are used to encrypt your data, and you can keep the keys secure in your own key vault."},
+        {"role": "user", "content": "Are those managed key secured stored?"}
+    ]
+)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(response)
+print(response['choices'][0]['message']['content'])
